3cf4956f2a2bf7fc338c157d55f909a4734a1d2f
Phathanan Paungpairoje <phathananpaungpairoje@Phathanans-MacBook-Pro.local>
2024-05-02 22:33:42 +0700

    Adding The Posts

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :title, presence: true, length: { minimum: 5, maximum: 1000 }
+end
